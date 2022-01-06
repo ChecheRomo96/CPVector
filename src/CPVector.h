f537ee4b38c8c97f911ec5bdb4c4e902ab07a95c
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2022-01-05 20:03:44 -0600

    Update CPVector.h

--- src/CPVector.h
+++ src/CPVector.h
@@ -2,9 +2,7 @@
 #define CROSS_PLATFORM_VECTOR_H
 
 	#include "CPVector_BuildSettings.h"
-	
 	#include "Sorting/CPVector_Sorting.h"
-
 	#include "CPVector_Template.h"
 
 #endif//CROSS_PLATFORM_STRING_H
