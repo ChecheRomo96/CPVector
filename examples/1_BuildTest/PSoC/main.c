995fbf654026b1cceeb590fd6624cda4c92f683c
José <josemanuel.romo.96@gmail.com>
2023-01-24 00:40:35 -0600

    Tested on multiple platforms
    Tested on Arduino Uno, Arduino Mega, PSoC Creator, Virtual Studio, XCode

---
+++ examples/1_BuildTest/PSoC/main.c
@@ -0,0 +1,24 @@
+extern "C"{
+    #include "project.h"
+}
+
+#include<CPVector.h>
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+    
+    UART_1_Start();
+    UART_1_PutString("This project uses CPVector version:");
+    UART_1_PutString(CPVECTOR_VERSION);
+    UART_1_PutChar('\n');
+    UART_1_PutChar('\r');
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
