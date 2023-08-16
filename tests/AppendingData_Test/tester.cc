c3d3fa98aa430304ba3c099e57c8509a047a1565
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-15 23:59:08 -0600

    Update tester.cc

--- tests/AppendingData_Test/tester.cc
+++ tests/AppendingData_Test/tester.cc
@@ -18,7 +18,7 @@ TEST(CPVectorTests, AppendingData) {
 	for(uint8_t i = 0; i < 100; i++ )
 	{
 		myVector.push_back(Data[i]);
-		std::cout<<(uint16_t)i<<" - "<myVector[i]<<std::endl;
+		std::cout<<(uint16_t)i<<" - "<<myVector[i]<<std::endl;
 		EXPECT_GT(myVector[i],i);
 	}
 
