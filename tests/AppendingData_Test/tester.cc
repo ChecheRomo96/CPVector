7728ca8deb9b85b9e67eb7cce73b538eb717a2c4
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-16 00:15:18 -0600

    Update tester.cc

--- tests/AppendingData_Test/tester.cc
+++ tests/AppendingData_Test/tester.cc
@@ -3,21 +3,20 @@
 #include <iostream>
 
 // Demonstrate some basic assertions.
-TEST(CPVectorTests, AppendingData) {
+TEST(CPVectorTests, push_back) {
 
-
-	uint8_t Data[100] = {};
-	for(uint8_t i = 0; i < 100; i++ )
+	uint8_t Data[UINT8_MAX] = {};
+	for(uint8_t i = 0; i < UINT8_MAX; i++ )
 	{
 		Data[i] = i;
 	}
 
 	CPVector::vector<uint8_t> myVector;
 
-
-	for(uint8_t i = 0; i < 100; i++ )
+	for(uint8_t i = 0; i < UINT8_MAX; i++ )
 	{
 		myVector.push_back(Data[i]);
+		ASSERT_EQ(myVector.size(),i+1)
 		std::cout<<(uint16_t)i<<" - "<<(uint16_t)myVector[i]<<std::endl;
 		EXPECT_EQ(myVector[i],i);
 	}
