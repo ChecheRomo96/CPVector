9271dc793ece45c1bc374bee1145d24b74c83cab
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-18 13:50:18 -0600

    Sorting Testing

---
+++ tests/Sorting/SortingTesting.cc
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <CPVector.h>
+#include <iostream>
+
+//////////////////////////////////////////////////////////////////////////////////
+// resize
+
+	TEST(SortingTesting, resize) {
+
+		
+		CPVector::vector<uint8_t> myVector(UINT8_MAX);
+		ASSERT_EQ(myVector.size(), UINT8_MAX);
+
+		for(uint8_t i = 0; i < myVector.size(); i++ )
+		{
+			myVector[i] = rand();
+		}
+
+		myVector.SortAscending();
+
+		for(uint8_t i = 0; i < myVector.size()-1; i++ )
+		{
+			EXPECCT_LE(myVector[i], myVector[i+1]);
+		}
+	}
+//
+//////////////////////////////////////////////////////////////////////////////////
