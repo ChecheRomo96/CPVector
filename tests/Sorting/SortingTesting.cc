4c3b6b66080537b4de10c68ca52d05bf2ff2f628
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-18 14:53:30 -0600

    Update SortingTesting.cc

--- tests/Sorting/SortingTesting.cc
+++ tests/Sorting/SortingTesting.cc
@@ -47,3 +47,53 @@
 	}
 //
 //////////////////////////////////////////////////////////////////////////////////
+//
+	struct TestingStruct
+	{
+		public:
+
+			uint8_t A;
+			uint8_t B;
+	};
+
+
+    int8_t TestingStruct_A_Ascending(const TestingStruct& Element, const TestingStruct& Pivot)
+    {
+        if(Element.A==Pivot.A){return CPVector::Sorting::Equal;}
+        else if(Element.A<Pivot.A){return CPVector::Sorting::Swap;}
+        else{return CPVector::Sorting::Ignore;}
+    }
+
+    int8_t TestingStruct_B_Ascending(const TestingStruct& Element, const TestingStruct& Pivot)
+    {
+        if(Element.B==Pivot.B){return CPVector::Sorting::Equal;}
+        else if(Element.B<Pivot.B){return CPVector::Sorting::Swap;}
+        else{return CPVector::Sorting::Ignore;}
+    }
+
+
+	TEST(SortingTesting, SortingList) {
+
+		CPVector::vector<TestingStruct> myVector(UINT8_MAX);
+		ASSERT_EQ(myVector.size(), UINT8_MAX);
+
+		for(uint8_t i = 0; i < myVector.size(); i++ )
+		{
+			myVector[i].A = rand();
+			myVector[i].B = rand();
+		}
+
+
+		CPVector::Sorting::SortingArray<TestingStruct> SortConfig;
+		SortConfig.Append(TestingStruct_A_Ascending);
+		SortConfig.Append(TestingStruct_B_Ascending);
+		
+		myVector.Sort(SortConfig);
+
+		for(uint8_t i = 0; i < myVector.size()-1; i++ )
+		{
+			EXPECT_GE(myVector[i], myVector[i+1]);
+		}
+	}
+//
+//////////////////////////////////////////////////////////////////////////////////
