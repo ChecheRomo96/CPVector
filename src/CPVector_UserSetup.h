46e09658b5142f4a44f3ab2447ca36000e575c8e
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-25 16:34:36 -0600

    Update CPVector_UserSetup.h

--- src/CPVector_UserSetup.h
+++ src/CPVector_UserSetup.h
@@ -1,6 +1,16 @@
 #ifndef CPVECTOR_USER_SETUP_H
 #define CPVECTOR_USER_SETUP_H
 
+
+
+	//////////////////////////////////////////////////////
+	// ESP32 Only
+		#if defined(ESP32)
+			#define CPVECTOR_USING_STD
+		#endif
+	//
+	//////////////////////////////////////////////////////
+
 	#define CPVECTOR_SORTING_ENABLED
 
 
