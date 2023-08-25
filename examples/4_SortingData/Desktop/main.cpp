4a730e81af6bb9046cf7fcc42f1ec7379c8fafd8
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-24 20:14:35 -0600

    Update main.cpp

--- examples/4_SortingData/Desktop/main.cpp
+++ examples/4_SortingData/Desktop/main.cpp
@@ -3,27 +3,29 @@
 
 int main()
 {
-    CPVector::vector<uint8_t> myVector1;
-	myVector1.resize(20);
+	#if defined(CPVECTOR_SORTING_ENABLED)
+	    CPVector::vector<uint8_t> myVector1;
+		myVector1.resize(20);
 
-	for (uint16_t i = 0; i < myVector1.size(); i++)
-	{
-		myVector1[i] = rand();
-		std::cout << (unsigned int)myVector1[i];
-		if (i < myVector1.size() - 1) { std::cout << ", "; }
-		else { std::cout << std::endl; }
-	}
-	
-	myVector1.sortAscending();
+		for (uint16_t i = 0; i < myVector1.size(); i++)
+		{
+			myVector1[i] = rand();
+			std::cout << (unsigned int)myVector1[i];
+			if (i < myVector1.size() - 1) { std::cout << ", "; }
+			else { std::cout << std::endl; }
+		}
+		
+		myVector1.sortAscending();
 
-	for (uint16_t i = 0; i < myVector1.size(); i++)
-	{
-		std::cout << (unsigned int)myVector1[i];
-		if (i < myVector1.size() - 1) { std::cout << ", "; }
-		else { std::cout << std::endl; }
-	}
+		for (uint16_t i = 0; i < myVector1.size(); i++)
+		{
+			std::cout << (unsigned int)myVector1[i];
+			if (i < myVector1.size() - 1) { std::cout << ", "; }
+			else { std::cout << std::endl; }
+		}
 
-	myVector1.clear();
-
+		myVector1.clear();
+	#endif
+		
     return 0;
 }
