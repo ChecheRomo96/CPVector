2afc9c65da3c65c42f2b1c36f2242e18c6ac7f8e
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-25 19:51:30 -0600

    Update CPVector_Sorting.h

--- src/Sorting/CPVector_Sorting.h
+++ src/Sorting/CPVector_Sorting.h
@@ -29,17 +29,32 @@
             /**
              * @brief Used to indicate that the objects being compared should be swapped.
              *
-             * Used in CPVector::vector<T> in order to reference a Sorting::Callback. A Sorting::Callback stores a pointer to a function that follows the required convention.
-             * The sorting function definition must recive two contstant references to a template type object, and must return a CPVector::Sorting::Result containing CPVector::Sorting::Equal, CPVector::Sorting::Swap, or CPVector::Sorting::Ignore.\n 
-             * Check CPVector::Sorting::Ascending and CPVector::Sorting::Descending as a reference.
+             * An object must be swapped if the element being compared should go before the pivot. For example, the function CPVector::Sort::Ascending() returns swap whenever Element < Pivot, hence Element goes before Pivot.
              */
 			static constexpr Result Swap = 1;
+
+            /**
+             * @brief Used to indicate that the objects being compared are equal under a specific criteria.
+             *
+             * If a comparison yields equal and there are no more criterions available, the elements are not swapped.
+             */
 			static constexpr Result Equal = 0;
+
+            /**
+             * @brief Used to indicate that the objects being compared should be ignored.
+             *
+             * An object must be swapped if the element being compared should go after the pivot. For example, the function CPVector::Sort::Ascending() returns ignore whenever Element > Pivot, hence Element goes after Pivot.
+             */
 			static constexpr Result Ignore = -1;
 		//
         ////////////////////////////////////////////////////////////////////////
         // Comparing functions templates
 
+            /**
+             * @brief Template function that enables sorting elements in an ascending manner.
+             *
+             * Works with any class that has operators > , < and ==.
+             */
 	        template <class T>
 	        Result Ascending(const T& Element, const T& Pivot)
 	        {
@@ -48,6 +63,11 @@
 	            else{return Sorting::Ignore;}
 	        }
 	        
+            /**
+             * @brief Template function that enables sorting elements in an descending manner.
+             *
+             * Works with any class that has operators > , < and == available.
+             */
 	        template <class T>
 	        Result Descending(const T& Element, const T& Pivot)
 	        {
