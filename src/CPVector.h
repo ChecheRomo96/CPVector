f5b4ab0d73f2d40ed7295ed44a623c5d9e537052
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-26 19:57:45 -0600

    Update CPVector.h

--- src/CPVector.h
+++ src/CPVector.h
@@ -1,3 +1,6 @@
+/** Cross Platform Vector main header
+ *  In this file the needed files are included depending on the build configuration.
+ */
 #ifndef CROSS_PLATFORM_VECTOR_H
 #define CROSS_PLATFORM_VECTOR_H
 
@@ -10,3 +13,8 @@
 	#include "CPVector_Template.h"
 
 #endif//CROSS_PLATFORM_STRING_H
+
+/** \example ./examples/1_BuildTest/Desktop/main.cpp
+ * This is an example of how to use the Example_Test class.
+ * More details about this example.
+ */
