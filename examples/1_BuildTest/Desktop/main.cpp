d3d3c0e87aac37970e7dd1f10d5c8a663f7947dc
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-26 23:41:29 -0600

    .

--- examples/1_BuildTest/Desktop/main.cpp
+++ examples/1_BuildTest/Desktop/main.cpp
@@ -4,5 +4,10 @@
 int main()
 {
     std::cout<<"This project uses CPVector version: "<<CPVECTOR_VERSION<<std::endl;
+    
+    #if defined(CPVECTOR_SORTING_ENABLED)
+    	std::cout<<"CPVector::Sorting Enabled."
+    #endif
+
     return 0;
 }
