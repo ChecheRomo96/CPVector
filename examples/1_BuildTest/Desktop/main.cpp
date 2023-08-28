e4d7abc658ef96ac2df0c0bb18c24ff649bfa654
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-28 14:41:21 -0600

    Update main.cpp

--- examples/1_BuildTest/Desktop/main.cpp
+++ examples/1_BuildTest/Desktop/main.cpp
@@ -9,5 +9,22 @@ int main()
     	std::cout<<"CPVector::Sorting Enabled."<<std::endl;
     #endif
 
+
+
+        CPVector::vector<CPVector::vector<uint8_t>> myVector(10, CPVector::vector<uint8_t>(2) );
+
+        auto myVector2 = CPVector::move(myVector);
+
+
+        myVector.push_back(CPVector::move(CPVector::vector<uint8_t>(1)));
+        myVector.emplace(CPVector::move(CPVector::vector<uint8_t>(5)), 0);
+
+
+        std::cout << myVector.size() << ", " << myVector2.size() << std::endl;
+        std::cout << myVector.capacity() << ", " << myVector2.capacity() << std::endl;
+
+        std::cout << (unsigned int)myVector[0].size() << std::endl;
+        std::cout << (unsigned int)myVector[1].size() << std::endl;
+
     return 0;
 }
