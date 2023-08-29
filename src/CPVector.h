52278b7934ccc371d71c1f1c3ddb85608601c6f3
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-29 04:49:59 -0600

    Update CPVector.h

--- src/CPVector.h
+++ src/CPVector.h
@@ -2,6 +2,18 @@
 #define CROSS_PLATFORM_VECTOR_H
 
 	#include "CPVector_BuildSettings.h"
+
+	
+    /**
+	 * @brief This namespace contains the implementation of CPVector::vector class.
+     *
+     * The Vector namespace provides a set of classes and functions for working with a vector, a dynamic array that can resize itself when needed. It offers functionalities similar to the standard vector container in C++ STL.
+     */
+	namespace CPVector
+	{
+
+	}
+
 	
 	#if defined (CPVECTOR_SORTING_ENABLED)
 		#include "Sorting/CPVector_Sorting.h"
