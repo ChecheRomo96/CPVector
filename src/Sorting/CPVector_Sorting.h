0d0d234976aeb563ca48cd792f98d48779beb1fd
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-08-29 04:50:10 -0600

    Update CPVector_Sorting.h

--- src/Sorting/CPVector_Sorting.h
+++ src/Sorting/CPVector_Sorting.h
@@ -1,56 +1,73 @@
 #ifndef CROSS_PLATFORM_VECTOR_SORTING_H
 #define CROSS_PLATFORM_VECTOR_SORTING_H
-	
+	/**
+     *  @defgroup Sorting Sorting
+     *  @brief Functionality related to sorting vectors.
+     */
+
+
+    /**
+     * @ingroup Sorting
+     * @brief Namespace containing the sorting functionality of CPVector::vector
+     *
+     * Any Result that is not Swap, Equal, or Ignore, will have no efect on the sorting functionality of the vector. 
+     */
 	namespace CPVector::Sorting
 	{
-		////////////////////////////////////////////////////////////////////////
-		// type used as return type for comparing functions
+      	////////////////////////////////////////////////////////////////////////
+      	// type used as return type for comparing functions
 
             /**
+             * @ingroup Sorting
              * @brief return type for comparing functions
              *
              * Any Result that is not Swap, Equal, or Ignore, will have no efect on the sorting functionality of the vector. 
              */
-	    	typedef int8_t Result;
+          	typedef int8_t Result;
 
             /**
+             * @ingroup Sorting
              * @brief Template callback type
              *
              * Used in CPVector::vector<T> in order to reference a Sorting::Callback. A Sorting::Callback stores a pointer to a function that follows the required convention.\n\n
              * The sorting function definition must recive two contstant references to a template type object, and must return a CPVector::Sorting::Result containing CPVector::Sorting::Equal, CPVector::Sorting::Swap, or CPVector::Sorting::Ignore.\n\n
              * Check CPVector::Sorting::Ascending and CPVector::Sorting::Descending as a reference.
              */
-	    	template<class T>
-	    	using Callback = Result(*)(const T&, const T&);
-	    //
-		////////////////////////////////////////////////////////////////////////
-		// Definitions
+          	template<class T>
+          	using Callback = Result(*)(const T&, const T&);
+        //
+      	////////////////////////////////////////////////////////////////////////
+      	// Definitions
 
             /**
+             * @ingroup Sorting
              * @brief Used to indicate that the objects being compared should be swapped.
              *
              * An object must be swapped if the element being compared should go before the pivot. For example, the function CPVector::Sort::Ascending() returns swap whenever Element < Pivot, hence Element goes before Pivot.
              */
-			static constexpr Result Swap = 1;
+      		static constexpr Result Swap = 1;
 
             /**
+             * @ingroup Sorting
              * @brief Used to indicate that the objects being compared are equal under a specific criteria.
              *
              * If a comparison yields equal and there are no more criterions available, the elements are not swapped.
              */
-			static constexpr Result Equal = 0;
+      		static constexpr Result Equal = 0;
 
             /**
+             * @ingroup Sorting
              * @brief Used to indicate that the objects being compared should not be swapped.
              *
              * An object must be swapped if the element being compared should go after the pivot. For example, the function CPVector::Sort::Ascending() returns ignore whenever Element > Pivot, hence Element goes after Pivot.
              */
-			static constexpr Result Ignore = -1;
-		//
+      		static constexpr Result Ignore = -1;
+        //
         ////////////////////////////////////////////////////////////////////////
         // Comparing functions templates
 
             /**
+             * @ingroup Sorting
              * @brief Template function that enables sorting elements in an ascending manner.
              *
              * Works with any class that has operators > , < and ==.
@@ -64,6 +81,7 @@
 	        }
 	        
             /**
+             * @ingroup Sorting
              * @brief Template function that enables sorting elements in an descending manner.
              *
              * Works with any class that has operators > , < and == available.
