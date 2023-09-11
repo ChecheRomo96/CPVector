690255568e18b53e7e6a1d080004be0770e4ceb9
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-09-11 16:35:44 -0600

    Update BasicFunctionalityTesting.cc

--- tests/BasicFunctionalityTesting.cc
+++ tests/BasicFunctionalityTesting.cc
@@ -4,11 +4,10 @@
 
 //////////////////////////////////////////////////////////////////////////////////
 //! @test
-//! #Description:\n
 //! This test case assesses the "reserve" functionality of the CPVector class.
 //! The purpose of this test is to verify that the "reserve" method correctly
 //! allocates memory to accommodate the specified capacity without affecting the
-//! size of the vector.\n
+//! size of the vector.\n\n
 //! The test is expected to pass if all assertions hold true, demonstrating that
 //! the "reserve" method correctly allocates memory as specified without changing
 //! the size of the vector.
