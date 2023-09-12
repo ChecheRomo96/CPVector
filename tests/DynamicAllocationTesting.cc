407fbd6b7fdc1dfca70e27d51ca4a0056709da43
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-09-11 23:05:51 -0600

    Update DynamicAllocationTesting.cc

--- tests/DynamicAllocationTesting.cc
+++ tests/DynamicAllocationTesting.cc
@@ -3,7 +3,15 @@
 #include <iostream>
 
 //////////////////////////////////////////////////////////////////////////////////
-// dynamicAllocation1
+//! @test
+//! This test case assesses the dynamic allocation and use of a CPVector pointer
+//! with the object constructor.
+//! The purpose of this test is to verify that dynamic memory allocation for a
+//! CPVector pointer and object construction using the constructor work as
+//! expected.\n\n
+//! The test is expected to pass if all assertions hold true, demonstrating that
+//! dynamic memory allocation and object construction for a CPVector pointer are
+//! successful.
 
 	TEST(DynamicAllocationTesting, VectorPointerToObject) {
 
@@ -38,64 +46,101 @@
 	}
 //
 //////////////////////////////////////////////////////////////////////////////////
-// dynamicAllocation2
+//! @test
+//! This test case assesses dynamic allocation and use of a CPVector pointer to an
+//! array of vectors.
+//! The purpose of this test is to verify that dynamic memory allocation for a
+//! CPVector pointer to an array of vectors and subsequent object construction and
+//! manipulation work as expected.\n\n
+//! The test is expected to pass if all assertions hold true, demonstrating that
+//! dynamic memory allocation and object manipulation for a CPVector pointer to an
+//! array of vectors are successful.
 
-    #if defined(CPVECTOR_USING_STD_VECTOR_ALLOCATION) || defined(CPVECTOR_USING_CPP)
-		TEST(DynamicAllocationTesting, VectorPointerToArray) {
+	TEST(DynamicAllocationTesting, VectorPointerToArray) {
+    #if defined(CPVECTOR_USING_STD_VECTOR_ALLOCATION) || defined(CPVECTOR_USING_CPP_ALLOCATION)
 
-			CPVector::vector<uint8_t>* myVectorptr = nullptr;
+		CPVector::vector<uint8_t>* myVectorptr = nullptr;
 
-			EXPECT_EQ((void*)myVectorptr,nullptr);
-			
-			myVectorptr = new CPVector::vector<uint8_t>[8];
+		EXPECT_EQ((void*)myVectorptr,nullptr);
+		
+		myVectorptr = new CPVector::vector<uint8_t>[8];
 
-			ASSERT_NE(myVectorptr,nullptr);
+		ASSERT_NE(myVectorptr,nullptr);
 
-			for(uint8_t i = 0; i < 8; i++){
-				myVectorptr[i] = CPVector::vector<uint8_t>(8);
-				ASSERT_EQ(myVectorptr[i].size(), 8);
+		for(uint8_t i = 0; i < 8; i++){
+			myVectorptr[i] = CPVector::vector<uint8_t>(8);
+			ASSERT_EQ(myVectorptr[i].size(), 8);
 
-			    for(uint8_t j = 0; j < 8; j++){    
-					myVectorptr[i][j] = (8*i) + j;
-			    }
-			}
-			
-			for(uint8_t i = 0; i < 8; i++){
-			    for(uint8_t j = 0; j < 8; j++){
-			        EXPECT_EQ(myVectorptr[i][j], (8*i) + j);
-			    }
-			}
-
-			delete[] myVectorptr;
+		    for(uint8_t j = 0; j < 8; j++){    
+				myVectorptr[i][j] = (8*i) + j;
+		    }
 		}
+		
+		for(uint8_t i = 0; i < 8; i++){
+		    for(uint8_t j = 0; j < 8; j++){
+		        EXPECT_EQ(myVectorptr[i][j], (8*i) + j);
+		    }
+		}
+
+		delete[] myVectorptr;
+	#elif defined(CPVECTOR_USING_C_ALLOCATION)
+
+		CPVector::vector<uint8_t>* myVectorptr = NULL;
+
+		myVectorptr = (CPVector::vector<uint8_t>*)calloc(8, sizeof(CPVector::vector<uint8_t>));
+
+		ASSERT_NE(myVectorptr, (void*)NULL);
+
+		for(uint8_t i = 0; i < 8; i++){
+			myVectorptr[i] = CPVector::vector<uint8_t>(8);
+
+		    for(uint8_t j = 0; j < 8; j++){    
+				myVectorptr[i][j] = (8*i) + j;
+		    }
+		}
+		
+		for(uint8_t i = 0; i < 8; i++){
+		    for(uint8_t j = 0; j < 8; j++){
+		        EXPECT_EQ(myVectorptr[i][j], (8*i) + j);
+		    }
+		    myVectorptr[i].CPVector::vector<uint8_t>::~vector();
+		}
+		free(myVectorptr);
+		myVectorptr = NULL;
+
 	#endif
+	}
 //
 //////////////////////////////////////////////////////////////////////////////////
-// dynamicAllocation3
+//! @test
+//! This test case assesses dynamic allocation and use of a CPVector containing
+//! CPVector elements.
+//! The purpose of this test is to verify that dynamic memory allocation for a
+//! CPVector containing CPVector elements, resizing, and subsequent element
+//! manipulation work as expected.\n\n
+//! The test is expected to pass if all assertions hold true, demonstrating that
+//! dynamic memory allocation, resizing, and element manipulation for a CPVector
+//! containing CPVector elements are successful.
 
 	TEST(DynamicAllocationTesting, VectorOfVectors) {
 		CPVector::vector<CPVector::vector<uint8_t>> myVector(8);
 
 		ASSERT_EQ(myVector.size(),8);
 
-		for(uint8_t i = 0; i < myVector.size(); i++)
-		{
+		for(uint8_t i = 0; i < myVector.size(); i++){
 			ASSERT_EQ(myVector[i].size(),0);
 
 			myVector[i].resize(8);
 			ASSERT_EQ(myVector[i].size(),8);
 
-			for(uint8_t j = 0; j < myVector[i].size(); j++)
-			{
+			for(uint8_t j = 0; j < myVector[i].size(); j++){
 				myVector[i][j] = (8*i) + j;
 				EXPECT_EQ(myVector[i][j], (8*i) + j);
 			}
 		}
 
-		for(uint8_t i = 0; i < myVector.size(); i++)
-		{
-			for(uint8_t j = 0; j < myVector[i].size(); j++)
-			{
+		for(uint8_t i = 0; i < myVector.size(); i++){
+			for(uint8_t j = 0; j < myVector[i].size(); j++){
 				myVector[i][j] = (8*i) + j;
 				EXPECT_EQ(myVector[i][j], (8*i) + j);
 			}
