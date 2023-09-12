f2469a0005b4941fe563fc9a6cb5f893cc195dc6
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-09-11 23:05:57 -0600

    Update OperatorTesting.cc

--- tests/OperatorTesting.cc
+++ tests/OperatorTesting.cc
@@ -3,7 +3,14 @@
 #include <iostream>
 
 //////////////////////////////////////////////////////////////////////////////////
-// Assignment Operator
+//! @test
+//! This test case assesses the assignment operator of the CPVector class.
+//! The purpose of this test is to verify that the assignment operator correctly
+//! assigns the values from one CPVector to another and ensures the size is
+//! correctly updated.\n\n
+//! The test is expected to pass if all assertions hold true, demonstrating that
+//! the assignment operator effectively assigns values from one CPVector to
+//! another and updates the size accordingly.
 
 	TEST(OperatorTesting, Assignment) {
 
@@ -18,7 +25,16 @@
 	}
 //
 //////////////////////////////////////////////////////////////////////////////////
-// Subscript Array Operator
+//! @test
+//! This test case assesses the subscript operator [] of the CPVector class for
+//! array-style element access.
+//! The purpose of this test is to verify that the subscript operator correctly
+//! allows access to individual elements of a CPVector, enabling reading and
+//! updating their values. It also checks the size of the vector to ensure it
+//! remains unchanged.\n\n
+//! The test is expected to pass if all assertions hold true, demonstrating that
+//! the subscript operator [] effectively enables array-style element access, and
+//! the size of the vector remains consistent.
 
 	TEST(OperatorTesting, SubscriptArray) {
 
@@ -33,7 +49,15 @@
 	}
 //
 //////////////////////////////////////////////////////////////////////////////////
-// Comparison Operators
+//! @test
+//! This test case assesses the comparison operators of the CPVector class.
+//! The purpose of this test is to verify that the equality and inequality operators
+//! correctly compare two CPVector instances for equality and inequality based on
+//! their contents. The test covers scenarios with empty and non-empty vectors,
+//! resizing, and changing element values to ensure the operators work as expected.\n\n
+//! The test is expected to pass if all assertions hold true, demonstrating that
+//! the comparison operators effectively compare CPVector instances based on their
+//! contents and return the correct results.
 
 	TEST(OperatorTesting, Comparison) {
 
